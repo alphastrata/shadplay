26654c08cc9294b2c95f4c01b69e0b6c3e3eada5
jer <alphastrata@gmail.com>
2023-10-11 08:30:40 +1100

    chore: add some scripts that I often use to git

--- scripts/reset-2d.py
+++ scripts/reset-2d.py
@@ -1,3 +1,10 @@
+TWO2_SHADER_STARTER = """
+
+/// ***************************** ///
+/// THIS IS THE DEFAULT 2D SHADER ///
+/// You can always get back to this with `python3 scripts/reset-2d.py` ///
+/// ***************************** ///
+
 #import bevy_pbr::mesh_vertex_output MeshVertexOutput
 #import bevy_sprite::mesh2d_view_bindings globals 
 #import shadplay::shader_utils::common NEG_HALF_PI, shaderToyDefault, rotate2D
@@ -18,3 +25,14 @@ fn fragment(in: MeshVertexOutput) -> @location(0) vec4<f32> {
 
     return vec4f(shaderToyDefault(t, uv), 1.0);
 }    
+    
+"""
+
+# Define the file path
+file_path = "./assets/shaders/myshader_2d.wgsl"
+
+# Open the file in write mode and replace its contents
+with open(file_path, "w") as file:
+    file.write(TWO2_SHADER_STARTER)
+
+print(f"Content in {file_path} has been replaced with the provided shader code.")
---
+++ scripts/search-bevy-codebase-for.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if the argument is provided
+if [ $# -ne 1 ]; then
+    echo "----------------------------------------------"
+    echo ""
+    ehco ">>>HELP"
+    echo "       Usage: $0 <search_query>"
+    echo ""
+    echo ""
+    echo "       Examples:"
+    echo "              ./scripts/search-bevy-codebase-for.sh mesh2d_vertex_output"
+    echo "              ./scripts/search-bevy-codebase-for.sh MeshVertexOutput"
+    echo ""
+    echo ""
+    echo "----------------------------------------------"
+    exit 1
+fi
+
+# Define the GitHub search URL for WGSL code in the Bevy repository
+search_query="$1"
+github_url="https://github.com/search?q=repo%3Abevyengine%2Fbevy+$search_query+language%3AWGSL&type=code&l=WGSL"
+
+# Open the URL in the default web browser
+xdg-open "$github_url"
