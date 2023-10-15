9b5294825f51d6bee0dd9311cc1d0966493fc604
jer <alphastrata@gmail.com>
2023-10-15 13:44:26 +1100

    wip: making a colour picker

--- src/ui.rs
+++ src/ui.rs
@@ -3,6 +3,90 @@ use bevy_egui::egui::epaint::Shadow;
 use bevy_egui::egui::{Align2, Color32, RichText, Rounding, Vec2};
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
+pub mod colour_picker_plugin {
+    use bevy::prelude::*;
+    use bevy_egui::egui::epaint::Shadow;
+    use bevy_egui::egui::{Align2, Color32, RichText, Rounding, Vec2};
+    use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
+    #[derive(Resource, Default, Debug, PartialEq)]
+    pub struct ColourPickerTool {
+        colour: [f32; 3],
+        open: bool,
+    }
+
+    #[derive(Resource, Default, Debug, PartialEq)]
+    struct Toggle {
+        open: bool,
+    }
+    impl Toggle {
+        fn flip(&mut self) {
+            self.open = !self.open;
+        }
+    }
+    /// Plugin:
+    /// Systems/Resources to allow a user to hit a hotkey --select a colour and have it copied to the system clipboard
+    pub struct ColourPickerPlugin;
+
+    impl Plugin for ColourPickerPlugin {
+        fn build(&self, app: &mut App) {
+            app.add_plugins(EguiPlugin);
+
+            app.insert_resource(ColourPickerTool::default());
+            app.insert_resource(Toggle::default());
+
+            app.add_systems(
+                Update,
+                (
+                    toggle_ui,
+                    ColourPickerTool::draw_ui
+                        .run_if(resource_exists::<ColourPickerTool>())
+                        .run_if(resource_equals(Toggle { open: true })),
+                ),
+            );
+        }
+    }
+
+    impl ColourPickerTool {
+        fn draw_ui(mut picker: ResMut<ColourPickerTool>, mut ctx: EguiContexts) {
+            egui::Window::new("Colour Picker")
+                .collapsible(false)
+                .resizable(false)
+                .frame(egui::Frame {
+                    inner_margin: egui::Margin {
+                        left: 3.0,
+                        right: 3.0,
+                        top: 3.0,
+                        bottom: 3.0,
+                    },
+                    outer_margin: egui::Margin {
+                        left: 0.0,
+                        right: 0.0,
+                        top: 0.0,
+                        bottom: 0.0,
+                    },
+                    rounding: Rounding::same(2.0),
+                    shadow: Shadow::NONE,
+                    fill: Color32::from_black_alpha(2),
+                    stroke: egui::Stroke::NONE,
+                })
+                .anchor(Align2::RIGHT_BOTTOM, Vec2::new(-35.0, -30.0))
+                .show(ctx.ctx_mut(), |ui| {
+                    ui.label(RichText::new("Colour Picker").color(Color32::WHITE));
+                    ui.color_edit_button_rgb(&mut picker.colour);
+                    debug!("{:?}", picker.colour);
+                });
+        }
+    }
+
+    /// System: Toggles the UI on/off
+    fn toggle_ui(input: Res<Input<KeyCode>>, mut picker: ResMut<Toggle>) {
+        if input.just_pressed(KeyCode::Tab) {
+            (*picker).flip()
+        }
+    }
+}
+
 /// Plugin:
 /// Systems/Resources etc to facilitate the help UI which will show the hotkey bindings.
 pub struct HelpUIPlugin;
@@ -44,6 +128,7 @@ fn toggle_help_ui(input: Res<Input<KeyCode>>, mut toggle: ResMut<HelpUIToggle>) 
         (*toggle).flip();
     }
 }
+
 /// System: draws the Help UI.
 /*
 to update run: `rg "input.just_pressed" -B 4`, the names of the functions should be indicative of what the binding does.
