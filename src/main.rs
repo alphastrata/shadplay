97e96bbdeccf2884b301a64aa6cf8594fec223b2
Jer <alphastrata@gmail.com>
2023-11-02 07:27:58 +0000

    long-lived app config closes !39
    * feat: long-lived user configs to close out [!39](https://github.com/alphastrata/shadplay/issues/39)

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use bevy::window::CompositeAlphaMode;
 
 use bevy::{
     asset::ChangeWatcher,
+    input::keyboard::KeyboardInput,
     prelude::*,
     sprite::Material2dPlugin,
     utils::Duration,
-    window::WindowLevel,
-    window::{Window, WindowPlugin, WindowResized},
+    window::{WindowLevel, WindowPlugin, WindowResized},
 };
 use bevy_panorbit_camera::PanOrbitCameraPlugin;
 
@@ -18,52 +18,46 @@ use shadplay::{
     drag_n_drop::{self, TexHandleQueue, UserAddedTexture},
     screenshot,
     shader_utils::{self, DragNDropShader},
+    system::UserConfig,
     texture_tooling,
     ui::colour_picker_plugin,
     utils::{self, AppState, MonitorsSpecs, Rotating, ShapeOptions, TransparencySet},
 };
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    // Get UserConfig for the Shadplay window dimensions, decorations toggle etc.
+    let user_config = UserConfig::load_from_toml(UserConfig::get_config_path())?;
+    let user_cfg_window = user_config.create_window_settings();
+
     let mut app = App::new();
 
     let shadplay = app
         .add_state::<AppState>()
-        .add_plugins((DefaultPlugins
-            .set(AssetPlugin {
-                watch_for_changes: ChangeWatcher::with_delay(Duration::from_millis(200)),
-                ..default()
-            })
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "shadplay".into(),
-                    resolution: (720.0, 480.0).into(),
-                    transparent: true,
-                    // Not well supported on these OSes
-                    #[cfg(any(target_os = "macos", target_os = "windows"))]
-                    decorations: true,
-                    #[cfg(target_os = "linux")]
-                    decorations: false,
-                    // Mac only
-                    #[cfg(target_os = "macos")]
-                    composite_alpha_mode: CompositeAlphaMode::PostMultiplied,
-
-                    // Sensible default
-                    window_level: WindowLevel::AlwaysOnTop,
+        .add_plugins((
+            DefaultPlugins
+                .set(AssetPlugin {
+                    watch_for_changes: ChangeWatcher::with_delay(Duration::from_millis(200)),
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(user_cfg_window), // From UserConfig
                     ..default()
                 }),
-                ..default()
-            }),))
+            //
+        ))
         .add_plugins(shader_utils::common::ShadplayShaderLibrary) // Something of a library with common functions.
         .add_plugins(colour_picker_plugin::ColourPickerPlugin)
         .add_plugins(MaterialPlugin::<shader_utils::YourShader>::default())
         .add_plugins(Material2dPlugin::<shader_utils::YourShader2D>::default())
         // Resources
         .insert_resource(MonitorsSpecs::default())
+        .insert_resource(user_config) //UserConfig
         .insert_resource(TexHandleQueue::default())
         .insert_resource(utils::ShadplayWindowDims::default())
         .insert_resource(ShapeOptions::default())
         .insert_resource(TransparencySet(true))
         .insert_resource(Rotating(false))
+        .insert_resource(ClearColor(Color::NONE))
         .add_plugins(PanOrbitCameraPlugin)
         //events:
         .add_event::<UserAddedTexture>()
@@ -74,8 +68,7 @@ fn main() {
         // 2D
         .add_systems(OnEnter(AppState::TwoD), utils::setup_2d)
         .add_systems(OnExit(AppState::TwoD), utils::cleanup_2d)
-        // All the time
-        .insert_resource(ClearColor(Color::NONE))
+        // Setups.
         .add_systems(PreStartup, utils::init_shapes)
         // 3d Cam Systems
         .add_systems(
@@ -105,6 +98,8 @@ fn main() {
                 utils::switch_level,
                 utils::toggle_transparency,
                 utils::toggle_window_passthrough,
+                UserConfig::runtime_updater.run_if(on_event::<KeyboardInput>()),
+                UserConfig::runtime_updater.run_if(on_event::<WindowResized>()),
             ),
         )
         // 2d Only Sytsems
@@ -124,4 +119,6 @@ fn main() {
     shadplay.add_plugins(HelpUIPlugin);
 
     shadplay.run();
+
+    Ok(())
 }
