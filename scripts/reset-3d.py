c01ff81a7332942f4b106b8ccdc474302e07cff7
jer <alphastrata@gmail.com>
2023-11-16 21:28:10 +1100

    fix: update scripts to reset shaders to their defaults

--- scripts/reset-3d.py
+++ scripts/reset-3d.py
@@ -16,7 +16,7 @@ THREE3_SHADER_STARTER = """//!
 const SPEED:f32 = 1.0;
 
 @fragment
-fn fragment(in: MeshVertexOutput) -> @location(0) vec4<f32> {
+fn fragment(in: VertexOutput) -> @location(0) vec4<f32> {
     // ensure our uv coords match shadertoy/the-lil-book-of-shaders
     let texture_uvs = in.uv;
 
