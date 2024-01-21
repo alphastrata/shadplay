ec983305aaae5c3cc92c4db6c4ea2ed4da7a8cd6
jer <alphastrata@gmail.com>
2024-01-22 07:15:08 +1100

    wip: keep a scratch sufrace in userConfig, pull and update it on resize events...

--- src/system/gif_maker.rs
+++ src/system/gif_maker.rs
@@ -1,15 +1,23 @@
+#![allow(unused_imports)]
 use bevy::prelude::*;
 use bevy::render::render_resource::{
     Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
 };
 use bevy::render::view::screenshot::ScreenshotManager;
+
 use bevy::render::view::RenderLayers;
+use bevy::window::WindowResized;
 
 pub struct GifMakerPlugin;
 
 impl Plugin for GifMakerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
+
+        app.add_systems(
+            Update,
+            size_gif_capture_surface.run_if(on_event::<WindowResized>()),
+        );
     }
 }
 /// Saves a screenshot continiously for ten seconds should you hit `return`.
@@ -23,9 +31,9 @@ impl Plugin for GifMakerPlugin {
 ///    ... etc
 ///```
 pub fn setup(
-    input: Res<Input<KeyCode>>,
-    user_config: Res<super::config::UserConfig>,
-    mut screenshot_manager: ResMut<ScreenshotManager>,
+    // input: Res<Input<KeyCode>>,
+    mut user_config: ResMut<super::config::UserConfig>,
+    // mut screenshot_manager: ResMut<ScreenshotManager>,
     mut images: ResMut<Assets<Image>>,
 ) {
     let (width, height) = user_config.window_dims;
@@ -54,5 +62,31 @@ pub fn setup(
     image.resize(size);
     let image_handle = images.add(image);
 
-    let gif_capture_pass = RenderLayers::layer(1);
+    user_config.gif_capture_surface = Some(image_handle);
+
+    // let gif_capture_pass = RenderLayers::layer(1);
+}
+
+pub fn size_gif_capture_surface(
+    user_config: Res<super::config::UserConfig>,
+    // mut screenshot_manager: ResMut<ScreenshotManager>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    let (width, height) = user_config.window_dims;
+
+    let Some(ref handle) = user_config.gif_capture_surface else {
+        return;
+    };
+
+    let Some(im) = images.get_mut(handle) else {
+        return;
+    };
+
+    let size = Extent3d {
+        width: width as u32,
+        height: height as u32,
+        ..default()
+    };
+
+    im.resize(size);
 }
