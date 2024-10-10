87fba95f4a7e07cc03d23b26cc8c185d12b62820
Jer <alphastrata@gmail.com>
2024-10-11 02:16:03 +1100

    update shaderbook (#97)
    * chore: cargo update
    
    * chore: bump deps
    
    * chore: clippy
    
    * chore: update shaderbook

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,7 @@ use shadplay::{plugin::ShadPlayPlugin, system::config::UserSession};
 fn main() {
     // Get UserConfig for the Shadplay window dimensions, decorations toggle etc.
     let path = UserSession::get_config_path();
-    let user_config = match UserSession::load_from_toml(path) {
-        Ok(config) => config,
-        Err(_) => UserSession::default(),
-    };
+    let user_config = UserSession::load_from_toml(path).unwrap_or_default();
     let user_cfg_window = user_config.create_window_settings();
 
     let mut app = App::new();
